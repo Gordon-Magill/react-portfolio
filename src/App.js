9f24cbc4e29a44d9336f742472c49e1e0d78b3d3
Gordon-Magill <gordon.magill@gmail.com>
2022-12-18 00:36:20 -0800

    Added basic page framework to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/Nav"
+import Hero from "./components/Hero"
+import Portfolio from "./components/Portfolio"
+import { useState } from "react";
 
 function App() {
+
+  const [darkMode, toggleDarkMode] = useState()
+
+
+  const compsciProjects = [
+    {
+      title: "Project A",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project B",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project C",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project D",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project E",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project F",
+      imgLink: logo,
+      extLink: "#",
+    },
+  ];
+
+  const chemEProjects = [
+    {
+      title: "Project A",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project B",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project C",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project D",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project E",
+      imgLink: logo,
+      extLink: "#",
+    },
+    {
+      title: "Project F",
+      imgLink: logo,
+      extLink: "#",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <Hero darkMode={darkMode}/>
+      <Portfolio projects={compsciProjects} darkMode={darkMode}/>
+      <Portfolio projects={chemEProjects} darkMode={darkMode}/>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
