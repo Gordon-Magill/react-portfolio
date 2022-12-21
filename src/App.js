4903e645ad29eef54d46419a37275e6b2a92bb0a
Gordon-Magill <gordon.magill@gmail.com>
2022-12-20 18:25:37 -0800

    Fixing broken tw config after purging old template content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Page from "./components/Page";
 // import Hero from "./components/Hero";
 // import Portfolio from "./components/Portfolio";
 // import AboutMe from "./components/AboutMe";
-import { useState } from "react";
 
 function App() {
   const [darkMode, toggleDarkMode] = useState();
   const [pages] = useState(["aboutMe", "portfolio", "contactMe", "resume"]);
 
   const [currentPage, setCurrentPage] = useState(pages[0]);
-
 
   return (
     <div className="container w-screen">
