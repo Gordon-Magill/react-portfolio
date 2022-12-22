d8f1742a829b4e55529f77c53ae16b7047469c7c
Gordon-Magill <gordon.magill@gmail.com>
2022-12-22 04:36:05 -0800

    Fixed transposed links between github and deployed pages

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,7 +14,7 @@ export default function Project({ project }) {
         ></img>
       </div>
       <div className="flex flex-row absolute bottom-0 right-0 bg-white/80 rounded-full m-1 items-center" id='linkContainer'>
-        <a href={project.extLink}>
+        <a href={project.ghLink}>
           <img
             src={ghLogoDark}
             className="w-5 h-5 md:w-10 md:h-10 m-1 ring ring-black rounded-full ring-inset "
@@ -24,14 +24,14 @@ export default function Project({ project }) {
         {project.extLink.length > 0 ? (
           <a
             className=" rounded-full m-2 p-1  ring ring-green-800 bg-green-400 hover:bg-green-500 focus:bg-green-600 text-xs md:text-base font-bold"
-            href={project.ghLink}
+            href={project.extLink}
           >
             Live page
           </a>
         ) : (
           <a
             className=" rounded-full m-2 p-1  ring ring-red-800 bg-red-400 hover:bg-red-500 text-xs md:text-base font-bold"
-            href={project.ghLink}
+            href={project.extLink}
           >
             No deployment
           </a>
